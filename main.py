8c1a07b9897cf2a9c7c5390621193e4fb8f67b56
Felix Doerrschuck <fdoerrsc@mail.uni-mannheim.de>
2021-10-06 14:16:58 +0200

    basic implementation to get user stories

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import spacy, coreferee
 from spacy import displacy
 
+import utils
+
 text = open("Texts/Model3-1.txt").read()
 
 nlp = spacy.load("en_core_web_sm")
@@ -19,49 +21,16 @@ doc = nlp(text)
 
 
 class Action:
-  def __init__(self, actor, action_token, *objects):
-    self.actor = actor
-    self.action_token = action_token
-    self.objects = objects
-
-def get_direct_ancestors(head_token, doc_):
-  ancestors = []
-  for token in doc_:
-    if token.head == head_token:
-      ancestors.append(token)
-  return ancestors
+    def __init__(self, actor, action_token, objects):
+        self.actor = actor
+        self.action_token = action_token
+        self.objects = objects
 
 
-def get_main_sentence(sent):
-  root_token = next(filter(lambda token: token.dep_ == 'ROOT', sent))
-  conjuncts = list(filter(lambda token: token.dep_ == 'conj', sent))
-  conjuncts = list(map(lambda token: token.text, conjuncts))
-  main_sent = []
-
-  for token in sent:
-    if (token.head == root_token
-        or token.head.text in conjuncts):
-      # remove adpositions and adverbs
-      if token.pos_ == 'ADP' or token.pos_ == 'ADV':
-        continue
-
-      # remove punctuation
-      if  token.pos_ == 'PUNCT':
-        continue
-
-      # remove adverbial clause modifiers for now
-      if token.dep_ == 'advcl':
-        continue
-
-      # remove empty sentences
-      if token.dep_ == 'ROOT' and token.tag_ == '_SP':
-        continue
-
-      # qualified for main sentence, add
-      main_sent.append(token)
-
-
-  return main_sent
+class ParticipantStory:
+    def __init__(self, actor, actions):
+        self.actor = actor
+        self.actions = actions
 
 
 # Extract actions from the text
@@ -70,53 +39,87 @@ actions = []
 
 for sent in doc.sents:
 
-  main_sent = get_main_sentence(sent)
-  actor = None
-  action = None
-  objects = []
+    main_sent = utils.get_main_sentence(sent)
+    if len(main_sent) == 0:
+        continue
 
-  for token in main_sent:
-    # filter out only main sentences
-    # print(token.text, end=" ")
-    if token.dep_ == 'nsubj':
-      actor = token
+    actor = None
+    action = None
+    objects = []
 
-    if token.dep_ == 'ROOT':
-      action = token
+    for token in main_sent:
+        # filter out only main sentences
+        # print(token.text, end=" ")
+        head_token = token.head
 
-    if token.dep_ == 'dobj' and token.head == action:
-      objects.append(token)
+        if token.dep_ == 'nsubj':
+            actor = token
 
-    print(
-      token.text + '(' + token.dep_ + ', ' + token.head.text + ')',
-      end=" ")
-  print('')
-  # print('----')
+        if token.dep_ == 'ROOT':
+            action = token
 
-  if actor is None and isinstance(previous_action, Action):
-    actor = previous_action.actor
+        if token.dep_ == 'dobj' and head_token == action:
+            objects.append(token)
 
-  current_action = Action(actor, action, objects)
+        # Check if sentence is passive and set passive object as actor
+        if head_token.dep_ == 'agent' \
+            and head_token.head == action and action.tag_ == 'VBN':
+            actor = token
 
-  actions.append(current_action)
+        # Add passive subject to actors
+        if token.dep_ == 'nsubjpass':
+            objects.append(token)
 
-  previous_action = current_action
 
-# print out all
+        print(
+            token.text + '(' + token.dep_ + ', ' + token.head.text + ')',
+            end=" ")
+    print('')
+    # print('----')
+
+    if actor is None and isinstance(previous_action, Action):
+        actor = previous_action.actor
+
+    current_action = Action(actor, action, objects)
+
+    actions.append(current_action)
+
+    previous_action = current_action
+
+print('----')
+
+# Remove the pipe that merges some phrases. This is needed to compare the actors
+# by removing some stop words
+nlp.remove_pipe('merge_entities')
+nlp.remove_pipe('merge_noun_chunks')
+
+participant_stories = []
+
 for action in actions:
-  if action.actor:
-    print('Action for ' + action.actor.text + ': ' + action.action_token.lemma_, action.objects)
+    # Check if there is already a participant story for the actor, if so just
+    # add the action to it
+    actor = nlp(action.actor.text)
+    participant_story = utils.find_participant_story_for_actor(actor, participant_stories)
+    action = action.action_token.lemma_ + ' ' \
+             + (action.objects[0].text if action.objects[0] else '')
 
+    if participant_story:
+        participant_story.actions.append(action)
+    else:
+        participant_story = ParticipantStory(actor, [action])
+        participant_stories.append(participant_story)
 
-# get similarity
-def similarity(subjects):
-  for subject1Index in range(len(subjects)):
-    subject1 = subjects[subject1Index]
+    # print(action.actor, action.action_token)
+    # if action.actor and action.action_token:
+    #     print(
+    #         'Action for ' + action.actor.text + ': ' + action.action_token.lemma_,
+    #         action.objects[0])
 
-    for subject2Index in range(subject1Index + 1, len(subjects)):
-      subject2 = subjects[subject2Index]
-      similarity = subject1.similarity(subject2)
+for participant_story in participant_stories:
+    print('--- ' + participant_story.actor.text + ' ---')
 
-      print(
-          'Similarity for ' + subject1.text + ' and ' + subject2.text + ': ' + str(
-              similarity))
+    for (number, participant_action) in enumerate(participant_story.actions):
+        print(str(number + 1) + '. ' + participant_action)
+
+    print('')
+    print('')
---
+++ utils.py
@@ -0,0 +1,70 @@
+def get_main_sentence(sent):
+    root_token = next(filter(lambda token: token.dep_ == 'ROOT', sent))
+    conjuncts = list(filter(lambda token: token.dep_ == 'conj', sent))
+    conjuncts = list(map(lambda token: token.text, conjuncts))
+    main_sent = []
+
+    for token in sent:
+        if token.head == root_token or token.head.text in conjuncts \
+            or token.dep_ == 'agent' or token.head.dep_ == 'agent':
+            # remove adpositions and adverbs
+            if token.pos_ == 'ADP' or token.pos_ == 'ADV':
+                continue
+
+            # remove punctuation
+            if token.pos_ == 'PUNCT':
+                continue
+
+            # remove adverbial clause modifiers for now
+            if token.dep_ == 'advcl':
+                continue
+
+            # remove empty sentences
+            if token.dep_ == 'ROOT' and token.tag_ == '_SP':
+                continue
+
+            # qualified for main sentence, add
+            main_sent.append(token)
+
+    return main_sent
+
+
+# get similarity
+def similarity(subjects):
+    for subject1Index in range(len(subjects)):
+        subject1 = subjects[subject1Index]
+
+        for subject2Index in range(subject1Index + 1, len(subjects)):
+            subject2 = subjects[subject2Index]
+            similarity = subject1.similarity(subject2)
+
+            print(
+                'Similarity for ' + subject1.text + ' and ' + subject2.text + ': ' + str(
+                    similarity))
+
+
+def get_direct_ancestors(head_token, doc_):
+    ancestors = []
+    for token in doc_:
+        if token.head == head_token:
+            ancestors.append(token)
+    return ancestors
+
+
+def compare_actors(actor1, actor2):
+    return remove_stop_words(actor1).upper() == remove_stop_words(actor2).upper()
+
+
+def remove_stop_words(phrase):
+    cleaned_phrase = ''
+    for token in phrase:
+        if not token.is_stop:
+            cleaned_phrase += token.text
+    return cleaned_phrase
+
+
+def find_participant_story_for_actor(actor, stories):
+    for story in stories:
+        if compare_actors(story.actor, actor):
+            return story
+    return None
